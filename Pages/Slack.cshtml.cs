d5bd84398e852cddde8f20bc6a063a1f644f4235
Myke Bates <contact@mykebates.com>
2019-09-09 04:30:32 -0500

    slack redirect

---
+++ Pages/Slack.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HorseStrap.Pages
+{
+    public class Slack : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
